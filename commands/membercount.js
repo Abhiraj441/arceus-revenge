5b2ff075e567335c3ca0085e2caec73fe1bafc92
RockstarElite <53997539+RockstarElite@users.noreply.github.com>
2019-10-20 20:47:50 +0530

    Update membercount.js

--- commands/membercount.js
+++ commands/membercount.js
@@ -1,5 +1,5 @@
 const { Client, RichEmbed } = require('discord.js');
-const guild = Client.guilds.get('631481205442740234');
+const guild = Client.guilds.get('627839622197805056');
 const userCount = guild.memberCount;
 const humanCount = guild.members.filter(member => !member.user.bot).size;
 const botCount = guild.members.filter(member => !member.user.human).size;
