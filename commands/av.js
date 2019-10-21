e16ba2baf81f3c827ce8a055c6ae5176bde14b4c
RockstarElite <53997539+RockstarElite@users.noreply.github.com>
2019-10-21 20:02:17 +0530

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -5,7 +5,7 @@ const { Client, RichEmbed } = require('discord.js');
         run: (client, message, args) => {
             const user = message.mentions.users.first() || message.author;
             function doRandHT() {
-                var rand = [`${user}`];
+                var rand = [user];
                 
                 return rand[Math.floor(Math.random()*rand.length)];
                 }
