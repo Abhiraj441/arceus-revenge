6cba939b28cd1b84bb5534e1621e9fc06e40b242
RockstarElite <53997539+RockstarElite@users.noreply.github.com>
2019-10-21 19:52:08 +0530

    Create av.js

---
+++ commands/av.js
@@ -0,0 +1,19 @@
+const config = require("../config.json")
+const { Client, RichEmbed } = require('discord.js');
+    module.exports = {
+        name:'dog',
+        run: (client, message, args) => {
+            const user = message.mentions.users.first() || message.author;
+            function doRandHT() {
+                var rand = [`${user}`];
+                
+                return rand[Math.floor(Math.random()*rand.length)];
+                }
+              
+            let rollembed = new RichEmbed()
+        .setColor("#15f153")
+        .setImage(doRandHT())
+        message.channel.send(rollembed);   
+    }
+        
+    }
