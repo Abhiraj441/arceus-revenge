ad4363481be475c0b74dde962d408382750a6c9c
RockstarElite <53997539+RockstarElite@users.noreply.github.com>
2019-10-22 19:09:14 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,1 +1,35 @@
-q
+const config = require("../config.json")
+
+const { Client, RichEmbed } = require('discord.js');
+
+    module.exports = {
+
+        name:'dog',
+
+        run: (client, message, args) => {
+          
+if (!message.mentions.users.size) {
+
+        const avatarAuthor = new Discord.RichEmbed()
+
+      .setColor(0x333333)
+
+      .setAuthor(message.author.username)
+
+      .setImage(message.author.avatarURL)
+
+        message.channel.send(avatarAuthor);
+
+        let mention = message.mentions.members.first();
+
+        const avatarMention = new Discord.RichEmbed()
+
+        .setColor(0x333333)
+
+        .setAuthor(mention.user.username)
+
+        .setImage(mention.user.avatarURL)
+
+        message.channel.send(avatarMention);
+  }
+          }
