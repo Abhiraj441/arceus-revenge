655eca9deec4ed289e26f679143eb905cb5b6ced
RockstarElite <53997539+RockstarElite@users.noreply.github.com>
2019-12-25 06:16:36 +0530

    Update membership.js

--- commands/membership.js
+++ commands/membership.js
@@ -12,24 +12,24 @@ AR Membership is a Membership for this server which grants you some special feat
      .addField(`__**AR Monthly Membership**__ (25k Credits)
 
 **Benefits:**`, `
-> :arrow_right:Weekly Membership giveaways!
-> :arrow_right:4x Giveaway Req and snipe bypass pass
-> :arrow_right:Free 5 invites in each Contest!
-> :arrow_right:Free Custom Role + Color
-> :arrow_right:Free Private Channel (all bots,no rules there!)
-> :arrow_right:Get 40% off in #❰🎊❱➤sponsor-req 
-> (Can use 2 times)
+:NeonR: Weekly Membership giveaways!
+:NeonR: 4x Giveaway Req and snipe bypass pass
+:NeonR: Free 5 invites in each Contest!
+:NeonR: Free Custom Role + Color
+:NeonR: Free Private Channel (all bots,no rules there!)
+:NeonR: Get 40% off in #❰🎊❱➤sponsor-req 
+(Can use 2 times)
      `)
      .addField(`__**AR Lifetime Membership**__ (75k Credits)
 
 **Benefits:**`, 
-`> :arrow_right:Access to!
-> :arrow_right:4x Giveaway Req bypass pass per month
-> :arrow_right:Free 7 invites in each Contest!
-> :arrow_right:Free Custom Role + Color
-> :arrow_right:Free Private Channel (all bots,no rules there!)
-> :arrow_right:Get 50% off in #❰🎊❱➤sponsor-req 
-> (Only Can Use 4 times)`)
+`:NeonR: Access to!
+:NeonR: 4x Giveaway Req bypass pass per month
+:NeonR: Free 7 invites in each Contest!
+:NeonR: Free Custom Role + Color
+:NeonR: Free Private Channel (all bots,no rules there!)
+:NeonR: Get 50% off in #❰🎊❱➤sponsor-req 
+(Only Can Use 4 times)`)
             .addField("__**Note:**__", `> 1. Rewards are only valid till Membership lasts
 > 2. DM only me to buy!`)
      .setFooter("If you want to buy DM Owner")
