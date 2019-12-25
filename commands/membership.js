ac0f832fd80922ce6f92d932430eeacf8b2f925e
RockstarElite <53997539+RockstarElite@users.noreply.github.com>
2019-12-25 06:14:28 +0530

    Update membership.js

--- commands/membership.js
+++ commands/membership.js
@@ -9,27 +9,30 @@ const { Client, RichEmbed } = require('discord.js');
         .addField(":crown:__What is AR Membership?__:thinking:", 
         `
 AR Membership is a Membership for this server which grants you some special features more than normal users for Monthly/Lifetime!:tada:`)
-     .addField("<:down_arrow:637934706855378944>__Benefits:__<:down_arrow:637934706855378944>", `
+     .addField(`__**AR Monthly Membership**__ (25k Credits)
 
-     :bell: Custom Role! (Hoised/Seprated)
-
-     :bell: Private Channel! (Includes all Bots & Add Upto 50 People)
-     
-     :bell: 3 Invites Per Invite Contest!
-     
-     :bell: Sniping Pass! 
-     
-     :bell: 50% Off in <#630721093463834624> Can use on 5 Items per month
-     
-     :bell: Membership Special Weekly Giveaways!
-     
-     :bell: Participate in Giveaways Without Meeting Req. (Except Giveaways Held By RAOG Not)
+**Benefits:**`, `
+> :arrow_right:Weekly Membership giveaways!
+> :arrow_right:4x Giveaway Req and snipe bypass pass
+> :arrow_right:Free 5 invites in each Contest!
+> :arrow_right:Free Custom Role + Color
+> :arrow_right:Free Private Channel (all bots,no rules there!)
+> :arrow_right:Get 40% off in #❰🎊❱➤sponsor-req 
+> (Can use 2 times)
      `)
-     .addField(":dollar:__Cost:__:dollar:", 
-`<:approved:636130484019134464> 50k for Monthly Membership!
-<:approved:636130484019134464> 200k for Lifetime Membership!
-<:approved:636130484019134464> To Purchase DM <@474529598504304641> or <@602829045956083733> ONLY.`)
-     .setFooter("If you want to buy with <@432616859263827988> or <@292953664492929025>, It will cost 1,000 Golds/Diamonds per Pokecord Credit")
+     .addField(`__**AR Lifetime Membership**__ (75k Credits)
+
+**Benefits:**`, 
+`> :arrow_right:Access to!
+> :arrow_right:4x Giveaway Req bypass pass per month
+> :arrow_right:Free 7 invites in each Contest!
+> :arrow_right:Free Custom Role + Color
+> :arrow_right:Free Private Channel (all bots,no rules there!)
+> :arrow_right:Get 50% off in #❰🎊❱➤sponsor-req 
+> (Only Can Use 4 times)`)
+            .addField("__**Note:**__", `> 1. Rewards are only valid till Membership lasts
+> 2. DM only me to buy!`)
+     .setFooter("If you want to buy DM Owner")
         message.channel.send(rollembed);   
     }
         
