97f09ad84a880a13588f2bef8f310d2b953f6896
RockstarElite <53997539+RockstarElite@users.noreply.github.com>
2019-12-25 06:24:16 +0530

    Update membership.js

--- commands/membership.js
+++ commands/membership.js
@@ -11,26 +11,25 @@ const { Client, RichEmbed } = require('discord.js');
 AR Membership is a Membership for this server which grants you some special features more than normal users for Monthly/Lifetime!:tada:`)
      .addField("__**AR Monthly Membership**__ (25k Credits)", `
 **Benefits:**
-:NeonR: Weekly Membership giveaways!
-:NeonR: 4x Giveaway Req and snipe bypass pass
-:NeonR: Free 5 invites in each Contest!
-:NeonR: Free Custom Role + Color
-:NeonR: Free Private Channel (all bots,no rules there!)
-:NeonR: Get 40% off in #❰🎊❱➤sponsor-req 
+<:NeonR2:659196667395964959> Weekly Membership giveaways!
+<:NeonR2:659196667395964959> 4x Giveaway Req and snipe bypass pass
+<:NeonR2:659196667395964959> Free 5 invites in each Contest!
+<:NeonR2:659196667395964959> Free Custom Role + Color
+<:NeonR2:659196667395964959> Free Private Channel (all bots,no rules there!)
+<:NeonR2:659196667395964959> Get 40% off in #❰🎊❱➤sponsor-req 
 (Can use 2 times)
      `)
      .addField("__**AR Lifetime Membership**__ (75k Credits)", 
 `**Benefits:**
-:NeonR: Access to!
-:NeonR: 4x Giveaway Req bypass pass per month
-:NeonR: Free 7 invites in each Contest!
-:NeonR: Free Custom Role + Color
-:NeonR: Free Private Channel (all bots,no rules there!)
-:NeonR: Get 50% off in #❰🎊❱➤sponsor-req 
+<:NeonR2:659196667395964959> Access to!
+<:NeonR2:659196667395964959> 4x Giveaway Req bypass pass per month
+<:NeonR2:659196667395964959> Free 7 invites in each Contest!
+<:NeonR2:659196667395964959> Free Custom Role + Color
+<:NeonR2:659196667395964959> Free Private Channel (all bots,no rules there!)
+<:NeonR2:659196667395964959> Get 50% off in #❰🎊❱➤sponsor-req 
 (Only Can Use 4 times)`)
-            .addField("__**Note:**__", `> 1. Rewards are only valid till Membership lasts
-> 2. DM only me to buy!`)
-     .setFooter("If you want to buy DM Owner")
+            .addField("__**Note:**__", ` <:NeonR2:659196667395964959> Rewards are only valid till Membership lasts
+<:NeonR2:659196667395964959> DM only <@602829045956083733> to buy!`)
         message.channel.send(rollembed);   
     }
         
